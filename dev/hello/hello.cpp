86b9bf107a1d737ec756ff6ce290e9aa88757748
colinandzxx <colinandzxx@163.com>
2019-07-31 09:22:52 +0800

    update test;

--- dev/hello/hello.cpp
+++ dev/hello/hello.cpp
@@ -1,9 +1,29 @@
-// #include <stdlib.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <vector>
 
-void hello() {
-    // int i32 = 0;
-    // uint u32 = 0;
-    printf("hello world!\n");
-    return;
+extern "C" {
+
+char* hello(int oh) {
+    int i32 = 0;
+    uint32_t u32 = 0;
+    i32 = 10;
+    u32 = 20;
+    float f1 = 0.0;
+    float f2 = i32 + u32 + f1 + oh;
+    char* str = "hello world";
+    char str1[2] = {};
+    memcpy(str1, str, strlen(str));
+    //printf("hello world!\n");     // not support at current !!
+
+    // std::vector<int> vec1;       // not support at current !!
+    // for (int i = 1; i < 10; i ++) {
+    //     vec1.push_back(i);
+    // }
+
+    return str;
+}
+
 }
